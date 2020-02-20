63275734a535b82a2fe210204e0035eb7f612d4b
GiancarloZ <gzabaleta48@gmail.com>
2020-02-20 12:03:25 -0500

    add start game button and timer, add images for card front

--- js-frontend/src/components/cards.js
+++ js-frontend/src/components/cards.js
@@ -1,4 +1,4 @@
-class Cards{
+class Cards {
     constructor(){
         this.cards = []
         this.adapter = new CardsAdapter()
@@ -10,7 +10,11 @@ class Cards{
         this.cardsContainer = document.getElementById("cards-container")
         this.newUserName = document.getElementById("new-user-name")
         this.newUserForm = document.getElementById("new-user-form")
+        const cards = document.querySelectorAll('.card');
+        const start = document.getElementById("start-game")
         this.newUserForm.addEventListener('submit', this.createUser.bind(this))
+        cards.forEach(card => card.addEventListener('click', this.flipCard));
+        start.addEventListener('click', this.startGame);
     }
 
     fetchAndLoadCards(){
@@ -27,7 +31,7 @@ class Cards{
 
     render(){
         const cardsContainer = document.getElementById("cards-container")
-        cardsContainer.innerHTML = this.cards.map(card => `<li>${card.state}</li>`).join("")
+        // cardsContainer.innerHTML = this.cards.map(card => `<li>${card.state}</li>`).join("")
     }
 
     createUser(u){
@@ -38,4 +42,35 @@ class Cards{
             this.users.push(new User(user))
         })
     }
+
+    flipCard() {
+        this.classList.toggle('flip');
+    }
+
+    startGame() {
+        console.log("clicked")
+        let timer = 59;
+        const display = document.querySelector('#time');
+        let defaultDisplay = display.textContent = "00:00";
+        let btn = document.getElementById("start-game");
+        var starter = setInterval(function () {
+            let seconds = timer;
+            display.textContent = "00:" + seconds;
+            --timer
+            
+            btn.disabled = true
+            if (timer === 54) {
+                console.log("55")
+                clearInterval(starter);
+                display.textContent = defaultDisplay
+                btn.disabled = false;
+            } 
+        }, 1000);
+    }
+    
+    // window.onload = function () {
+    //     var oneMinute = 60,
+    //         display = document.querySelector('#time');
+    //     startGame(oneMinute, display);
+    // };
 }
