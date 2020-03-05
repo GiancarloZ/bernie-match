a23161a13db9e49467351908271dbfcce6638492
giancarloz <gzabaleta48@gmail.com>
2020-03-05 12:00:18 -0500

    fix bugs in styles

--- js-frontend/src/components/app.js
+++ js-frontend/src/components/app.js
@@ -1,7 +1,7 @@
 class App{
     constructor(){
         this.users = new Users()
-        // this.games = new Games()
+        this.games = new Games()
         // this.cards = new Cards()
     }
 }
